eb69f9f2589e2b0d0d1ccc2bedb7855f15639948
TooManyLoops <10404844@ucn.dk>
2024-12-02 14:17:50 +0100

    testMaterial updated
    update til test class

--- test/model/TestMaterial.java
+++ test/model/TestMaterial.java
@@ -1,5 +1,16 @@
 package model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TestMaterial {
 
+	@Test
+	void testMaterialConstructor() {
+		
+	}
+	
+	
+	
 }
