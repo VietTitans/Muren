25d93895a58de7ffcf31766e4b85175550d9277e
Kuskeren <166589219+Kuskeren@users.noreply.github.com>
2024-12-03 10:41:05 +0100

    Connection til DB
    Samt relevant test

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -1,5 +1,127 @@
 package db;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import controller.DataAccessException;
+
+
+public class DBConnection {
+	private Connection connection = null;
+	private static DBConnection dbConnection;
+
+	private static final String DBNAME = "DMA-CSD-V24_10519153";
+	private static final String SERVERNAME = "hildur.ucn.dk";
+	private static final int PORTNUMBER = 1433;
+	private static final String USERNAME = "DMA-CSD-V24_10519153";
+	private static final String PASSWORD = "Password1!";
+
+	public DBConnection() throws DataAccessException {
+
+		String urlString = String.format("jdbc:sqlserver://%s:%s;databaseName=%s;encrypt=false", SERVERNAME, PORTNUMBER,
+				DBNAME);
+		try {
+			connection = DriverManager.getConnection(urlString, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			throw new DataAccessException(String.format("Could not connect to database %s@%s:%d user %s", DBNAME,
+					SERVERNAME, PORTNUMBER, USERNAME), e);
+		}
+	}
+
+	public static synchronized DBConnection getInstance() throws DataAccessException {
+		if (dbConnection == null) {
+			dbConnection = new DBConnection();
+		}
+		return dbConnection;
+	}
+
+	public void startTransaction() throws DataAccessException {
+		try {
+			connection.setAutoCommit(false);
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new DataAccessException("Could not start transaction.", e);
+		}
+	}
+
+	public void commitTransaction() throws DataAccessException {
+		try {
+			try {
+				connection.commit();
+			} catch (SQLException e) {
+				throw e;
+				// e.printStackTrace();
+			} finally {
+				connection.setAutoCommit(true);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException("Could not commit transaction", e);
+		}
+	}
+
+	public void rollbackTransaction() throws DataAccessException {
+		try {
+			try {
+				connection.rollback();
+			} catch (SQLException e) {
+				throw e;
+				// e.printStackTrace();
+			} finally {
+				connection.setAutoCommit(true);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException("Could not rollback transaction", e);
+		}
+	}
+
+	public int executeInsertWithIdentity(String sql) throws DataAccessException {
+		int res = -1;
+		try (Statement s = connection.createStatement()) {
+			res = s.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+			if (res > 0) {
+				ResultSet rs = s.getGeneratedKeys();
+				rs.next();
+				res = rs.getInt(1);
+			}
+			// s.close(); -- the try block does this for us now
+
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new DataAccessException("Could not execute insert (" + sql + ").", e);
+		}
+		return res;
+	}
+
+	public int executeInsertWithIdentity(PreparedStatement ps) throws DataAccessException {
+		// requires prepared statement to be created with the additional argument PreparedStatement.RETURN_GENERATED_KEYS  
+		int res = -1;
+		try {
+			res = ps.executeUpdate();
+			if (res > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				rs.next();
+				res = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new DataAccessException("Could not execute insert", e);
+		}
+		return res;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void disconnect() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- test/db/TestDBConnection.java
+++ test/db/TestDBConnection.java
@@ -1,5 +1,30 @@
 package db;
 
-public class TestDBConnection {
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controller.DataAccessException;
+
+class TestDBConnection {
+	private DBConnection connection;
+
+	
+	
+	@BeforeEach
+	void setup() throws DataAccessException, SQLException {
+		connection = DBConnection.getInstance();
+		
+	}
+	@Test
+	void testConnection() throws SQLException {
+		DatabaseMetaData dma =connection.getConnection().getMetaData();
+		assertTrue(dma != null);
+	}
 
 }
