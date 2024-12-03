b879bbb0252c8aac5caa39eaf89d996ed9cafe36
Rosielyn15 <166709926+Rosielyn15@users.noreply.github.com>
2024-12-03 12:57:22 +0100

    Created CustomerDB class

--- src/db/CustomerDBIF.java
+++ src/db/CustomerDBIF.java
@@ -1,10 +1,11 @@
 package db;
 
+import controller.DataAccessException;
 import model.Customer;
 
 public interface CustomerDBIF {
 	
 	
-	public Customer findCustomerByPhoneNo(String PhoneNo);
+	public Customer findCustomerByPhoneNo(String PhoneNo, boolean fullAssertion) throws DataAccessException;
 
 }
