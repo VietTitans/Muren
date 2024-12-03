a4c9f8cf98d73863351256a450463d9ff4d3bf3f
Rosielyn15 <166709926+Rosielyn15@users.noreply.github.com>
2024-12-03 13:15:19 +0100

    Created DB classes for Log Subclasses

---
+++ src/db/HourLogDB.java
@@ -0,0 +1,18 @@
+package db;
+
+
+import model.HourLog;
+
+public class HourLogDB implements HourLogDBIF {
+
+	public HourLogDB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public HourLog saveHourLog(HourLog hourLog, int orderId) {
+		
+		return null;
+	}
+
+}
---
+++ src/db/MaterialLogDB.java
@@ -0,0 +1,17 @@
+package db;
+
+import model.MaterialLog;
+
+public class MaterialLogDB implements MaterialLogDBIF {
+
+	public MaterialLogDB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MaterialLog saveMaterialLog(MaterialLog materialLog, int orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
