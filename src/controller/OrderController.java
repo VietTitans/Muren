67cf696aa3f70c2ce75f951b967625e65385c5e5
Rosielyn15 <166709926+Rosielyn15@users.noreply.github.com>
2024-12-03 10:15:13 +0100

    Created and finished methods for OrderController

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,9 +1,59 @@
 package controller;
 
-public class OrderController {
+import model.Customer;
+import model.Employee;
+import model.HourLog;
+import model.Material;
+import model.MaterialLog;
+import model.Order;
+import db.OrderDBIF;
+import db.OrderDB;
 
+public class OrderController {
+	private Order currentOrder;
+	private LogController logController;
+	private EmployeeController employeeController;
+	private MaterialController materialController;
+	private OrderDBIF orderInterface;
+
+	
 	public OrderController() {
-		// TODO Auto-generated constructor stub
+		logController = new LogController();
+		employeeController = new EmployeeController();
+		materialController = new MaterialController();
+	}
+	
+	
+	public void registerOrder(Employee employee) {
+		currentOrder = new Order(employee);	
+	}
+	
+	public Customer findAndAddCustomerByPhoneNo(String PhoneNo) throws GeneralException {
+		CustomerController customerController = new CustomerController();
+		Customer foundCustomer =  customerController.findCustomerByPhoneNo(PhoneNo);
+		currentOrder.addCustomerToOrder(foundCustomer);
+		return foundCustomer;
+	}
+	
+	public Material findAndAddMaterialByPhoneNo(int materialNo, int quantity) {
+		Material foundMaterial = materialController.findMaterialByMaterialNo(materialNo);
+		MaterialLog newLog = logController.addMaterialToLog(foundMaterial, quantity);
+		currentOrder.addMaterialLogToOrder(newLog);
+		return foundMaterial;
+	}
+	
+	public Employee findEmployeeByEmployeeId(int employeeId) throws GeneralException {
+		Employee employee = employeeController.findEmployeeByEmployeeId(employeeId);
+		return employee;
+	}
+	
+	public void addWorkHours(Employee employee, double hours) {
+		HourLog newLog = logController.AddEmployeeToHourLog(employee, hours);
+		currentOrder.addHourLogToOrder(newLog);		
+	}
+	
+	public void saveOrder() {
+		orderInterface.saveOrder(currentOrder);
 	}
 
 }
