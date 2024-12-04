be17faf30fe95788efe302c3e7cabe978254f4b6
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-05 00:42:21 +0100

    Update TestCustomerDB.java

--- test/db/TestCustomerDB.java
+++ test/db/TestCustomerDB.java
@@ -2,8 +2,6 @@ package db;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.NoSuchElementException;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
