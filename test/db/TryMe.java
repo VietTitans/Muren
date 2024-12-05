89ae9221db79cb28359bfb70d12ce5e74dc203bb
Kuskeren <166589219+Kuskeren@users.noreply.github.com>
2024-12-05 09:56:35 +0100

    Update TryMe.java
    Corrected wrong testData

--- test/db/TryMe.java
+++ test/db/TryMe.java
@@ -21,7 +21,7 @@ public class TryMe {
 		emp.setEmployeeId(1);
 		Price price = new Price(BigDecimal.valueOf(1.00));
 		MaterialDescription matdes = new MaterialDescription("Ting");
-		Material mat =  new Material (1, "ds", matdes , price, price);
+		Material mat =  new Material (1001, "ds", matdes , price, price);
 		MaterialLog mlog = new MaterialLog(emp,mat,1);
 		
 		MaterialLogDB db = new MaterialLogDB();
