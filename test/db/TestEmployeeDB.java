9dcd0f38192adf455937591440faffdb81b3c0d9
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-05 20:24:33 +0100

    Implement testEmployeeDoesntExists(). (Forgot to write this from earliere commit)

--- test/db/TestEmployeeDB.java
+++ test/db/TestEmployeeDB.java
@@ -57,7 +57,7 @@ class TestEmployeeDB {
 	}
 	
 	@Test
-	void testEmployeeDoesntExists() throws DataAccessException {
+	void testEmployeeDoesntExists() throws DataAccessException { 
 		//Using NullPointerException to test for null
 		//Arrange
 		NullPointerException exceptionThrown = assertThrows(NullPointerException.class, () -> {
