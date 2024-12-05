90b3aed3ec0fd1c371c55a3a8343f877747cc04a
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-06 00:53:31 +0100

    Review TestOrderDB
    Refactor for better readability

--- test/db/TestOrderDB.java
+++ test/db/TestOrderDB.java
@@ -6,7 +6,9 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,69 +27,75 @@ class TestOrderDB {
 
 	static OrderDB orderDB;
 	private DBConnection connection;
-	
-	@BeforeEach
+
+	@BeforeAll
 	void setUp() throws Exception {
-		//Resets the database to a known state 
-		ResetDB.main(null);
 		orderDB = new OrderDB();
 		connection = DBConnection.getInstance();
 		connection.getConnection();
 	}
-	
-	@AfterEach
+
+	@AfterAll
 	void tearDown() throws DataAccessException {
 		connection.disconnect();
 	}
 
+	@BeforeEach
+	void setUpBeforeEach() throws Exception {
+		// Resets the database to a known state
+		ResetDB.main(null);
+	}
+
 	@Test
-	void testSaveOrderToDatabaseMainsuccess() throws DataAccessException, SQLException, GeneralException {
-		//Arrange
-		Employee emp = new Employee();
-		emp.setEmployeeId(1);
-		Order order = new Order(emp);
-		Price price = new Price(BigDecimal.valueOf(1.00));
-		MaterialDescription materialDescription = new MaterialDescription("Ting");
-		Material material =  new Material (1001, "ds", materialDescription , price, price);
+	void testSaveOrder() throws DataAccessException, SQLException, GeneralException {
+		// Arrange
 		Customer customer = new Customer();
 		customer.setCustomerId(1);
-		MaterialLog materialLog = new MaterialLog(emp,material,1);
 		
+		Employee emp = new Employee();
+		emp.setEmployeeId(1);
+		
+		Price purchasePrice = new Price(BigDecimal.valueOf(1.00));
+		Price salesPrice = new Price(BigDecimal.valueOf(2.00));
+		
+		MaterialDescription materialDescription = new MaterialDescription("Ting");
 		MaterialLogDB materialLogDB = new MaterialLogDB();
+		Material material = new Material(1001, "ds", materialDescription, purchasePrice, salesPrice); 
+		MaterialLog materialLog = new MaterialLog(emp, material, 1);
 		materialLogDB.saveMaterialLog(materialLog, 1);
-		
+
 		HourLog hourlog = new HourLog(emp, BigDecimal.valueOf(1));
 		HourLogDB hourLogDB = new HourLogDB();
 		hourLogDB.saveHourLog(hourlog, 1);
 		
+		Order order = new Order(emp);
 		order.addCustomerToOrder(customer);
-		
 		order.addHourLogToOrder(hourlog);
 		order.addMaterialLogToOrder(materialLog);
-		
+
 		OrderDB orderDB = new OrderDB();
 		order.setDeadLine(LocalDate.now());
-		
+		// Act
 		int returnedKey = orderDB.saveOrder(order);
-	
-	//Assert
-	assertEquals(6,returnedKey);
-	
+		// Assert
+		assertEquals(6, returnedKey);
 	}
+
 	@Test
-	void SaveWithInvalidinput() throws DataAccessException, SQLException {
+	void SaveWithInvalidInput() throws DataAccessException, SQLException {
+		// Arrange
 		Employee employee = new Employee();
 		employee.setEmployeeId(1000);
 		Order order = new Order(employee);
 		OrderDB orderDB = new OrderDB();
 
-		
 		NullPointerException exceptionThrown = assertThrows(NullPointerException.class, () -> {
-			//Act
+			// Act
 			orderDB.saveOrder(order);
 			throw new IllegalArgumentException("Customer not found");
 		});
-	assertEquals("java.lang.NullPointerException:",exceptionThrown);
+		// Assert
+		assertEquals("java.lang.NullPointerException:", exceptionThrown);
 	}
 
 }
