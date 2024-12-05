df6aeb0302695d45e1e3539b597f09df95cb9307
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-05 23:17:50 +0100

    Implement test for class method and test for null

--- test/db/TestHourLogDB.java
+++ test/db/TestHourLogDB.java
@@ -2,23 +2,62 @@ package db;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import controller.DataAccessException;
+import model.Employee;
+import model.HourLog;
 
 class TestHourLogDB {
 
-	@BeforeEach
+	static HourLogDB hourLogDB;
+	private DBConnection connection;
+
+	@BeforeAll
 	void setUp() throws Exception {
+		hourLogDB = new HourLogDB();
+		connection = DBConnection.getInstance();
+		connection.getConnection();
+		
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@AfterAll
+	void tearDown() throws DataAccessException {
+		connection.disconnect();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSaveHourLog() throws DataAccessException {
+		//Arrange
+		Employee employee = new Employee();
+		BigDecimal workedHours = new BigDecimal("2.0");
+		HourLog expectedResult = new HourLog(employee, workedHours);
+		int orderId = 1; 
+		//Act
+		hourLogDB.saveHourLog(expectedResult, orderId);
+		//Assert
+		//TODO: Implement getHourLog()
+		HourLog result = hourLogDB.getHourLog(orderId);
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	void testSaveHourLogDoesntExist() throws DataAccessException {
+		//Using NullPointerException to test for null
+		//Arrange
+		int orderId = 665;
+		NullPointerException exceptionThrown = assertThrows(NullPointerException.class, () -> {
+			//Act
+			//TODO: Implement getHourLog()
+			hourLogDB.getHourLog(orderId);
+			throw new IllegalArgumentException("Hourlog not found");
+		});
+		//Assert
+		assertEquals("Hourlog not found", exceptionThrown.getMessage());
 	}
 
 }
