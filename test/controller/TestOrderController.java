4285fb28ea6b66bc14f43e27f08c4ed46f8d544d
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-06 10:10:08 +0100

    Add TestOrderController. Move limitTest to OrderController

---
+++ test/controller/TestOrderController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestOrderController {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testOverMaxStockLimit() throws DataAccessException {
+		//TODO:STUBS
+	}
+	
+	@Test
+	void testWithInStockLimit() throws DataAccessException {
+		//TODO:STUBS
+	}
+	
+	@Test
+	void testUnderMinStockLimit() throws DataAccessException {
+		//TODO:STUBS
+	}
+
+}
