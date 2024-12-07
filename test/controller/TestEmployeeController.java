dcf18d3a3cfcfed93af2036132629ca2b2417e51
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-07 22:37:57 +0100

    Create TestEmployeeController.java

---
+++ test/controller/TestEmployeeController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Employee;
+
+class TestEmployeeController {
+	private EmployeeController employeeController;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		employeeController = new EmployeeController();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testFindEmployeeByEmployeeId() throws GeneralException, DataAccessException {
+		//Arrange
+		/*Employee exists in the database
+		Michael Scott 
+		CPR: 1234567890	
+		personId: 9
+		*/
+		//Act
+		int validEmployeeId = 9;
+		Employee result = employeeController.findEmployeeByEmployeeId(validEmployeeId, false);
+		//Assert
+		String expectedFName = "Michael";
+		String expectedLName = "Scott";
+		String expectedCPR = "1234567890";
+		String expectedEmployeeId = "9";
+		assertEquals(expectedFName, result.getfName());
+		assertEquals(expectedLName, result.getlName());
+		assertEquals(expectedCPR, result.getCpr());
+		assertEquals(expectedEmployeeId, result.getEmployeeId());
+	}
+	
+	@Test
+	void testEmployeeDoesntExists() throws NullPointerException {
+		//Arrange
+		NullPointerException exceptionThrown = assertThrows(NullPointerException.class, () -> {
+			//Act
+			int invalidEmployeeId = 45249;
+			employeeController.findEmployeeByEmployeeId(invalidEmployeeId, false);
+			throw new NullPointerException("Employee not found");
+		});
+		//Assert
+		assertEquals("Employee not found", exceptionThrown.getMessage());
+	}
+
+
+}
