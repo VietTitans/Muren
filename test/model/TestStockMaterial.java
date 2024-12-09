89ca814b70f792324098159f379da2575388593c
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-09 12:28:41 +0100

    Add boundry test

--- test/model/TestStockMaterial.java
+++ test/model/TestStockMaterial.java
@@ -1,20 +1,16 @@
 package model;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.DataAccessException;
-import db.DBConnection;
-import db.MaterialDB;
 
 public class TestStockMaterial {
 
@@ -37,43 +33,114 @@ public class TestStockMaterial {
 	@Test
 	public void testAddStockReservation() {
 		//Arrange
-		StockMaterial stockMaterial = new StockMaterial(123, "cement", materialDescription, purchasePrice, salesPrice, 15, 75, 30);
-		StockReservation stockReservation = new StockReservation(20, LocalDateTime.now(), 7, 7, false);
+		StockMaterial stockMaterial = new StockMaterial(123, "cement", null, null, null, 15, 75, 30);
+		StockReservation stockReservation = new StockReservation(1, LocalDateTime.now());
 		//Act
 		stockMaterial.addStockReservation(stockReservation);
 		//Assert
 		assertEquals(1, stockMaterial.getStockReservations().size());
 	}
 	
+	//Boundry testing addReservation: Over available amount
 	@Test
-	public void testUpdateAvailableAmount() {
+	public void testReservationOverAvailableLimitThrowsException() {
 		//Arrange
-		StockMaterial stockMaterial = new StockMaterial(123, "cement", materialDescription, purchasePrice, salesPrice, 15, 75, 30);
-		StockReservation stockReservation = new StockReservation(20, LocalDateTime.now(), 7, 7, false);
-		//Act
-		stockMaterial.updateAvailableAmount(stockReservation);
+		ArrayList<Price> salesPrices = new ArrayList<>();
+		Price salesPrice = new Price(null);
+		salesPrices.add(0, salesPrice);
+		ArrayList<Price> purchasePrices = new ArrayList<>();
+		Price purchasePrice = new Price(null);
+		purchasePrices.add(0, purchasePrice);
+		
+		int availableAmount = 50;
+		StockMaterial stockMaterial = new StockMaterial(1001, "cement", null, salesPrices, purchasePrices, 15, 75, availableAmount);
+		int reserveAmount = 51;
+		StockReservation stockReservation = new StockReservation(reserveAmount, LocalDateTime.now());
+
+		Exception exceptionThrown = assertThrows(Exception.class, () -> {
+			//Act
+			stockMaterial.addStockReservation(stockReservation);
+			throw new Exception("Cannot reserve over available amount");
+		});
+		String result = exceptionThrown.getMessage();
 		//Assert
-		assertEquals(10, stockMaterial.getAvailableAmount());
-	}
-	
-	@Test
-	public void testCalculatedAvailableAmount() {
-		//TODO: STUBS
-		//Arrange
-		//StockReservation, cement with 
-		//Act
-		//Assert
+		assertEquals("Cannot reserve over available amount", result);
 		
 	}
 	
-//	//Consider to implement this?
-//	@Test
-//	public void testFindStockReservation() {
-//		//TODO: STUBS
-//		//Arrange
-//		//Act
-//		//Assert
-//	}
-//	
+	//Boundry testing addReservation: Zero input
+	@Test
+	public void testReservationUnderAvailableLimitThrowsException() {
+		//Arrange
+		ArrayList<Price> salesPrices = new ArrayList<>();
+		Price salesPrice = new Price(null);
+		salesPrices.add(0, salesPrice);
+		ArrayList<Price> purchasePrices = new ArrayList<>();
+		Price purchasePrice = new Price(null);
+		purchasePrices.add(0, purchasePrice);
+		
+		int availableAmount = 50;
+		StockMaterial stockMaterial = new StockMaterial(1001, "cement", null, salesPrices, purchasePrices, 15, 75, availableAmount);
+		int reserveAmount = 0;
+		StockReservation stockReservation = new StockReservation(reserveAmount, LocalDateTime.now());
+
+		Exception exceptionThrown = assertThrows(Exception.class, () -> {
+			//Act
+			stockMaterial.addStockReservation(stockReservation);
+			throw new Exception("Cannot reserve the required amount");
+		});
+		String result = exceptionThrown.getMessage();
+		//Assert
+		assertEquals("Cannot reserve the required amount", result);
+		
+	}
 	
+	//Boundry testing addReservation: Negative input
+	@Test
+	public void testReservationMinusUnderAvailableLimitThrowsException() {
+		//Arrange
+		ArrayList<Price> salesPrices = new ArrayList<>();
+		Price salesPrice = new Price(null);
+		salesPrices.add(0, salesPrice);
+		ArrayList<Price> purchasePrices = new ArrayList<>();
+		Price purchasePrice = new Price(null);
+		purchasePrices.add(0, purchasePrice);
+		
+		int availableAmount = 50;
+		StockMaterial stockMaterial = new StockMaterial(1001, "cement", null, salesPrices, purchasePrices, 15, 75, availableAmount);
+		int reserveAmount = -1;
+		StockReservation stockReservation = new StockReservation(reserveAmount, LocalDateTime.now());
+
+		Exception exceptionThrown = assertThrows(Exception.class, () -> {
+			//Act
+			stockMaterial.addStockReservation(stockReservation);
+			throw new Exception("Cannot reserve the required amount");
+		});
+		String result = exceptionThrown.getMessage();
+		//Assert
+		assertEquals("Cannot reserve the required amount", result);
+		
+	}
+
+	@Test
+	public void testCalculateAvailableAmount() {
+		//Arrange
+		ArrayList<Price> salesPrices = new ArrayList<>();
+		Price salesPrice = new Price(null);
+		salesPrices.add(0, salesPrice);
+		ArrayList<Price> purchasePrices = new ArrayList<>();
+		Price purchasePrice = new Price(null);
+		purchasePrices.add(0, purchasePrice);
+		
+		int availableAmount = 50;
+		StockMaterial stockMaterial = new StockMaterial(1001, "cement", null, salesPrices, purchasePrices, 15, 75, availableAmount);
+		int reserveAmount = 20;
+		StockReservation stockReservation = new StockReservation(reserveAmount, LocalDateTime.now());
+		stockMaterial.addStockReservation(stockReservation);
+		//Act
+		stockMaterial.calculatedAvailableAmount();
+		//Assert
+		assertEquals(30, stockMaterial.getAvailableAmount());
+	}
+
 }
