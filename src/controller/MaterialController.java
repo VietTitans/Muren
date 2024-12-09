19a56e337abc6a471e19014323b6ec30627cb735
VietTitans <43970143+VietTitans@users.noreply.github.com>
2024-12-09 13:11:35 +0100

    Update MaterialController.java

--- src/controller/MaterialController.java
+++ src/controller/MaterialController.java
@@ -12,6 +12,6 @@ public class MaterialController {
 	}
  
 public Material findMaterialByMaterialNo(int materialNo, boolean fullAssociation) throws DataAccessException {
-	return materialDBIF.findMaterialByMaterialNo(materialNo,fullAssociation);
+	return materialDBIF.findMaterialByMaterialNo(materialNo);
 	}
 }
