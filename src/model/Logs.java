c34715042b7988ee384c3e7d5ab7def40611f382
TooManyLoops <10404844@ucn.dk>
2025-01-06 00:01:39 +0100

    logs update

--- src/model/Logs.java
+++ src/model/Logs.java
@@ -17,11 +17,11 @@ public abstract class Logs {
 		this.logTimeStamp = madeAtTime; 
 	}
 
-	public Employee getEmployeeSubClass() {
+	public Employee getEmployee() {
 		return employee;
 	}
 	
-	public LocalDateTime getTimeStampSubClass() {
+	public LocalDateTime getTimeStamp() {
 		return logTimeStamp;
 	}
 	
